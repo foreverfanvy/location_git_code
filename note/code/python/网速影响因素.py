75e9091acdac8e832f1180fe1833e570ebddd3aa
foreverfanvy <3075514079@qq.com>
2024-07-04 18:25:25 +0800

    code upload
    上传了之前写的一些代码

--- note/code/python/网速影响因素.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import xlrd
-
-file_location = "C:\\Users\\Administrator\\Desktop\\数据.xlsx"
-data = xlrd.open_workbook(file_location)
-
-df = pd.DataFrame(np.random.randn(3, 10), )
